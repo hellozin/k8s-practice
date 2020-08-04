4cd6bb14c72eb43f3ccba4d74cbbf5db756c0774
hellozin <paul26375@gmail.com>
2020-08-04 23:38:49 +0900

    [데모서버] 간단한 요청 처리 추가

--- demo/src/main/java/com/kakao/demo/DemoApplication.java
+++ demo/src/main/java/com/kakao/demo/DemoApplication.java
@@ -1,10 +1,20 @@
 package com.kakao.demo;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequiredArgsConstructor
 public class DemoApplication {
+
+    @GetMapping("/knock")
+    public String greeting() {
+        return "Hi, there.";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
