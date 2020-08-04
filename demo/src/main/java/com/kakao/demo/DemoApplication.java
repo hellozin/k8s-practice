e383492d0e1ba9d6b599605647d6ad42a11eb357
hellozin <paul26375@gmail.com>
2020-08-04 23:39:13 +0900

    [데모서버] 불필요한 lombok 제거

--- demo/src/main/java/com/kakao/demo/DemoApplication.java
+++ demo/src/main/java/com/kakao/demo/DemoApplication.java
@@ -1,6 +1,5 @@
 package com.kakao.demo;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@RequiredArgsConstructor
 public class DemoApplication {
 
     @GetMapping("/knock")
