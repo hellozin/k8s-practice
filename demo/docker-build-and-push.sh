2c566b496f0fe21c61d89034cbf4c9f3a768df22
hellozin <paul26375@gmail.com>
2020-09-13 17:27:26 +0900

    환경 변수 컨테이너에 주입

---
+++ demo/docker-build-and-push.sh
@@ -0,0 +1,8 @@
+# maven package
+mvn package
+
+# docker build
+docker build -t hellozin/demo .
+
+# docker push
+docker push hellozin/demo
--- demo/src/main/java/com/kakao/demo/DemoApplication.java
+++ demo/src/main/java/com/kakao/demo/DemoApplication.java
@@ -1,7 +1,9 @@
 package com.kakao.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,13 +11,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoApplication {
 
-    @GetMapping("/knock")
+    @GetMapping("/")
     public String greeting() {
         return "Hi, there.";
     }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Service
+    class Runner implements CommandLineRunner {
+        @Override
+        public void run(String... args) {
+            String env = System.getenv("hello.env");
+            System.out.println("=== hello.env : [" + env + "] ===");
+        }
     }
 
 }
